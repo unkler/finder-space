9b87a067bb22f7bd90298b192eeb767c0ddca53c
unkler <poru@add-natural.com>
2022-09-21 03:01:48 +0900

    いいね機能を実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,17 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+//vue.js
+import { createApp } from 'vue/dist/vue.esm-bundler'; 
+import ArticleLike from './components/ArticleLike.vue'
+
+const app = createApp({
+    components: {
+        ArticleLike,
+    }
+});
+app.mount('#app');
+
+
+
